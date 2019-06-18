896061489f4c2f9367bcfb12f8bc047930f5b6fa
CRONWMMM <kimmmiy@sina.cn>
2019-06-18 22:35:54 +0800

    重新打包 dist 文件

--- example/index.js
+++ example/index.js
@@ -10,23 +10,11 @@ import picture from './pic.jpg'
 const root = document.getElementById('root')
 
 ReactDOM.render(
-    <div>
-
-        <ReactPictureViewer className="viewport"
-                            contain
-                            center
-                            minimum={0.4}
-                            maximum={2}>
-            <img src={picture} alt="picture" draggable={false}/>
-        </ReactPictureViewer>
-
-        <ReactPictureViewer className="viewport"
-                            id="safd"
-                            contain
-                            center
-                            minimum={0.4}
-                            maximum={5}>
-            <img src={picture} alt="picture" draggable={false}/>
-        </ReactPictureViewer>
-    </div>
+    <ReactPictureViewer className="viewport"
+                        contain
+                        center
+                        minimum={0.4}
+                        maximum={2}>
+      <img src={picture} alt="picture" draggable={false}/>
+    </ReactPictureViewer>
 , root)
